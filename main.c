7d793f06619206977fa3a287652d8cafdd808d31
Bekhruz Niyazov <bekhruzsniyazov@outlook.com>
2021-03-24 10:54:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 		// getting the filename
 		filename = argv[1];
 		// getting the section and the key
-        char sectionKey[100];
-        strcpy(sectionKey, argv[2]);
+		char sectionKey[100];
+		strcpy(sectionKey, argv[2]);
 		char *token = strtok(sectionKey, ".");
 		int counter = 0;
 		while (token != NULL) {
@@ -22,28 +22,27 @@ int main(int argc, char *argv[])
 			{
 				strcpy(section, token);
 				token = strtok(NULL, ".");
-                counter++;
+				counter++;
 			}
 			else
 			{
 				strcpy(key, token);
-                token = NULL;
+				token = NULL;
 			}
 		}
-
 	}
 	else
 	{
 		printf("Usage: ./name filename.ini section.key\n");
 		return 0;
 	}
-
+	
 	if (access(filename, F_OK) != 0)
 	{
 		printf("Unable to open %s\n", filename);
 		return 0;
 	}
-
+	
 	char *contents = 0;
 
 	FILE *file = fopen(filename, "r");
@@ -76,26 +75,27 @@ int main(int argc, char *argv[])
 	// splitting the contents into lines
 	while (tk != NULL)
 	{
-        if (!strcmp(tk, section))
-        {
-            found = 1;
-        }
-        else if (found)
-        {
-	        char *words[] = {};
-	        int counter = 0;
-            char *word = strtok(tk, " = ");
-            while (word != NULL) {
-	            words[counter++] = word;
-	            word = strtok(NULL, " = ");
-            }
-	        if (!strcmp(words[0], key))
-            {
-				printf("The value is %s\n", words[1]);
-	            free(contentsPointer);
-	            return 1;
+		if (!strcmp(tk, section))
+		{
+			found = 1;
+        	}
+		else if (found)
+		{
+			char *words[] = {};
+			int counter = 0;
+			char *word = strtok(tk, " = ");
+			while (word != NULL)
+			{
+				words[counter++] = word;
+				word = strtok(NULL, " = ");
 			}
-        }
+			if (!strcmp(words[0], key))
+			{
+				printf("The value is %s\n", words[1]);
+				free(contentsPointer);
+				return 1;
+			}
+		}
 		tk = strtok_r(NULL, "\n", &saveptr);
 	}
 
